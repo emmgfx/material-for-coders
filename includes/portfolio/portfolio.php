2230500e51f5e67b899def3a60f8e12930d91009
Josep Viciana <emmgfx@gmail.com>
2015-10-24 14:34:24 +0200

    Project images editor working. Needs to be sanitized.

--- includes/portfolio/portfolio.php
+++ includes/portfolio/portfolio.php
@@ -45,49 +45,192 @@ function page_meta_boxes()
     );
 }
 
-function render_m4c_portfolio_images_metabox(){
+function render_m4c_portfolio_images_metabox($post){
 ?>
 <div class="uploader">
-	<a href="#" class="upload_image_button">Add images to project</a>
-	<div class="images-list">
+	<?php
+	wp_nonce_field( 'm4c_portfolio_images_metabox_data', 'm4c_portfolio_images_metabox_nonce' );
+	$images_json = get_post_meta( $post->ID, '_m4c_portfolio_images_order', true );
+	if(!is_array(@json_decode($images_json, true)))
+		$images_json = json_encode(array());
+	?>
 
+	<p><a href="#" class="upload_image_button button button-primary">Add images to project</a></p>
+	<div class="images-list">
+		<?php foreach(json_decode($images_json) as $attachment_id): ?>
+
+			<?php
+			$attachment_meta = wp_get_attachment_metadata($attachment_id);
+			if($attachment_meta == false)
+				continue;
+			?>
+			<div class="image" data-id="<?php echo $attachment_id; ?>">
+				<img src="<?php echo wp_get_attachment_thumb_url($attachment_id); ?>" />
+				<a href="#" class="button delete">Remove</a>
+			</div>
+		<?php endforeach; ?>
 	</div>
+
+	<input type="hidden" id="m4c_portfolio_images_order" name="m4c_portfolio_images_order" value="<?php echo esc_attr( $images_json ); ?> " />
 </div>
+<style>
+.images-list{
+	margin-top: 20px;
+	overflow: auto;
+}
+.images-list .image{
+	margin: 0 15px 15px 0;
+    padding: 5px;
+    background: #FFF;
+    border: 1px solid #DCDCDC;
+	border-radius: 2px;
+	float: left;
+}
+.images-list .image img{
+	width: 150px;
+	height: 150px;
+}
+.images-list a.delete{
+	display: block;
+	text-align: center;
+}
+</style>
 <script>
 
-var file_frame;
+jQuery(document).ready(function() {
 
-jQuery('.upload_image_button').live('click', function( event ){
+	var $ = jQuery;
 
-	event.preventDefault();
+	var file_frame;
 
-	if ( file_frame ) {
-		file_frame.open();
-		return;
-	}
+	$(document).on('click', '.upload_image_button', function( event ){
 
-	file_frame = wp.media.frames.file_frame = wp.media({
-		title: jQuery( this ).data( 'uploader_title' ),
-		button: {
-			text: jQuery( this ).data( 'uploader_button_text' ),
-		},
-		multiple: true
-	});
+		event.preventDefault();
 
-	file_frame.on( 'select', function() {
-		attachments = file_frame.state().get('selection').toJSON();
-
-		console.log(attachments[0]);
-
-		for(i = 0; i < attachments.length; i++){
-			jQuery(".uploader .images-list").append('<img src="' + attachments[i].sizes.thumbnail.url+ '" />');
+		if ( file_frame ) {
+			file_frame.open();
+			return;
 		}
 
+		file_frame = wp.media.frames.file_frame = wp.media({
+			title: jQuery( this ).data( 'uploader_title' ),
+			button: {
+				text: jQuery( this ).data( 'uploader_button_text' ),
+			},
+			multiple: true
+		});
+
+		file_frame.on( 'select', function() {
+			attachments = file_frame.state().get('selection').toJSON();
+
+			for(i = 0; i < attachments.length; i++){
+
+				var image = '<img src="' + attachments[i].sizes.thumbnail.url+ '" />';
+				var bt_delete = '<a href="#" class="button delete">Remove</a>';
+				var container = '<div class="image" data-id="' + attachments[i].id + '">' + image + bt_delete + '</div>'
+
+				$(".uploader .images-list").append(container);
+			}
+
+			updateImagesJSONField();
+
+		});
+
+		file_frame.open();
 	});
 
-	file_frame.open();
+	$(document).on('click', '.uploader .images-list div.image a.button.delete', function(){
+		$(this).closest('div.image').hide(500, function(){
+			$(this).remove();
+			updateImagesJSONField();
+		});
+		return false;
+	});
+
+	$( ".images-list" ).sortable({
+		update: function( event, ui ) {
+			updateImagesJSONField();
+		}
+	});
+
+	function getImagesJSON(){
+
+		var image_ids = [];
+
+		$('.images-list div.image').each(function(index, value){
+			var id = $(this).attr('data-id');
+			image_ids.push(id);
+		});
+
+		return JSON.stringify(image_ids);
+	}
+
+	function updateImagesJSONField(){
+		$('#m4c_portfolio_images_order').val(getImagesJSON());
+	}
+
 });
+
 </script>
 <?
 }
+
+function m4c_portfolio_save_metadata( $post_id ) {
+
+	/*
+	 * We need to verify this came from our screen and with proper authorization,
+	 * because the save_post action can be triggered at other times.
+	 */
+
+	// Check if our nonce is set.
+	if ( ! isset( $_POST['m4c_portfolio_images_metabox_nonce'] ) ) {
+		echo 'nonce not set';
+		return;
+	}
+
+	// Verify that the nonce is valid.
+	if ( ! wp_verify_nonce( $_POST['m4c_portfolio_images_metabox_nonce'], 'm4c_portfolio_images_metabox_data' ) ) {
+		echo 'nonce not verified';
+		return;
+	}
+
+	// If this is an autosave, our form has not been submitted, so we don't want to do anything.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		echo 'doing autosave';
+		return;
+	}
+
+	// Check the user's permissions.
+	if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			echo 'current user cant edit this page';
+			return;
+		}
+
+	} else {
+
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			echo 'current user cant edit this post';
+			return;
+		}
+	}
+
+	/* OK, it's safe for us to save the data now. */
+
+	// Make sure that it is set.
+	if ( ! isset( $_POST['m4c_portfolio_images_order'] ) ) {
+		echo 'm4c_portfolio_images_order not set';
+		return;
+	}
+
+	// Sanitize user input.
+	$my_data = sanitize_text_field( $_POST['m4c_portfolio_images_order'] );
+
+	// var_dump($my_data);
+
+	// Update the meta field in the database.
+	update_post_meta( $post_id, '_m4c_portfolio_images_order', $my_data );
+}
+add_action( 'save_post', 'm4c_portfolio_save_metadata' );
 ?>
