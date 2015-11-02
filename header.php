27192fe45fe19e75edc5de56be92e47f9c68f7a8
Josep Viciana <emmgfx@gmail.com>
2015-11-02 16:56:09 +0100

    Using the cache of the browser

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
+<?php
+header("Cache-Control: must-revalidate");
+header("Expires: ".gmdate ("D, d M Y H:i:s", time() + 60*60*24*7)." GMT");
+?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
