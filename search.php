8dcf5398176016bfd67e52e018b12f8aea4a2655
Carlos Peña Mañas <carlos@vasili.es>
2015-12-05 14:01:04 +0100

    hide sidebar if not posts found

--- search.php
+++ search.php
@@ -2,11 +2,12 @@
 
 <?php
 $option = array(
-	'sidebar_active' => intval(get_option('sidebar-active')) == 1,
-	'show_featured_index' => intval(get_option('show-featured-index')) == 1,
-	'show_featured_single' => intval(get_option('show-featured-single')) == 1,
-	'show_excerpt_in_lists' => intval(get_option('show-excerpt-in-lists')) == 1,
-);
+		'sidebar_active' 		=> intval(get_option('sidebar-active')) 		== 1,
+		'show_featured_index' 	=> intval(get_option('show-featured-index')) 	== 1,
+		'show_featured_single' 	=> intval(get_option('show-featured-single')) 	== 1,
+		'show_excerpt_in_lists'	=> intval(get_option('show-excerpt-in-lists'))	== 1,
+	);
+if($wp_query->found_posts === 0): $option['show_not_found'] = intval(1); endif;
 ?>
 
 <div class="big-title">
@@ -16,6 +17,9 @@ $option = array(
 </div>
 
 <div class="container">
+	<?php
+	if(!isset($option['show_not_found'])):
+	?>
 	<div class="row">
 		<div class="<?php echo ($option['sidebar_active'] ? 'col-md-9 col-sm-8' : 'col-md-8 col-md-offset-2'); ?>">
 
@@ -56,6 +60,16 @@ $option = array(
 		?>
 
 	</div>
+	<?php else: ?>
+		<br />
+		<div align="center">
+			<img src="<?php echo get_bloginfo('template_directory');?>/assets/img/cloud.svg" width="250" />
+		</div>
+		<br />
+		<div align="center">
+			<a href="<?php echo home_url(); ?>" class="btn btn-lg btn-primary">&larr; Volver a la portada</a>
+		</div>
+	<?php endif; ?>
 </div>
 
 
