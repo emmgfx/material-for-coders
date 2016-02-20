d0e597078f84c2eba8b552ca6c91f145d0954773
Josep Viciana <emmgfx@gmail.com>
2016-02-20 11:11:10 +0100

    Small change, preparing the new home page

--- portfolio_list_item.php
+++ portfolio_list_item.php
@@ -5,11 +5,13 @@ if(has_post_thumbnail()):
 
 else:
 
-	$images_json = get_post_meta( $post->ID, '_m4c_portfolio_images_order', true );
+	$images_json = get_post_meta( get_the_ID(), '_m4c_portfolio_images_order', true );
+
 	if(!is_array(@json_decode($images_json, true)))
 		$images_json = json_encode(array());
 
 	$images_array = json_decode($images_json);
+
 
 	if(isset($images_array[0])):
 		$poster_meta = wp_get_attachment_metadata($images_array[0]);
@@ -31,7 +33,7 @@ $project_categories = wp_get_post_terms( $post->ID, 'project_categories' );
 		<h3 class="project-title"><?php the_title(); ?></h3>
 		<ul class="project-categories list-unstyled list-inline">
 			<?php foreach($project_categories as $category): ?>
-				<li><a href="<?php echo get_site_url(); ?>/projects/category/<?php echo $category->slug; ?>">#<?php echo $category->name; ?></a></li> <?php //print_r($category); ?>
+				<li><a href="<?php echo get_site_url(); ?>/projects/category/<?php echo $category->slug; ?>">#<?php echo $category->name; ?></a></li>
 			<?php endforeach; ?>
 		</ul>
 	</div>
