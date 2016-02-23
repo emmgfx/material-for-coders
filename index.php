0b36d04e1cebbb82800de4c4bcf29fb6959d0033
Josep Viciana <emmgfx@gmail.com>
2016-02-23 18:59:50 +0100

    Test with infinite scroll

--- index.php
+++ index.php
@@ -19,39 +19,40 @@ $option = array(
 <div class="container">
 	<div class="row">
 		<div class="<?php echo ($option['sidebar_active'] ? 'col-md-8 col-sm-8' : 'col-md-8 col-md-offset-2'); ?>">
+			<div id="articles">
+				<?php if(have_posts()): while(have_posts()): the_post(); ?>
 
-			<?php if(have_posts()): while(have_posts()): the_post(); ?>
-
-			<?php
-			$post_classes = array('article-wrapper');
-			if($option['sidebar_active'])
-				$post_classes[] = 'sidebar-active';
-			?>
-
-			<div id="post-<?php the_ID(); ?>" <?php post_class($post_classes); ?>>
-				<?PHP
-				if(has_post_thumbnail() && $option['show_featured_index']):
-					echo '<a href="'.get_the_permalink().'">';
-					the_post_thumbnail('custom_1', array( 'class'	=> "img-rounded img-responsive center-block featured"));
-					echo '</a>';
-				else:
-					echo '<br />';
-				endif;
+				<?php
+				$post_classes = array('article-wrapper');
+				if($option['sidebar_active'])
+					$post_classes[] = 'sidebar-active';
 				?>
-				<h2><a href="<?PHP the_permalink(); ?>"><?PHP the_title(); ?></a></h2>
 
-				<?PHP get_template_part( 'context' ); ?>
+				<div id="post-<?php the_ID(); ?>" <?php post_class($post_classes); ?>>
+					<?PHP
+					if(has_post_thumbnail() && $option['show_featured_index']):
+						echo '<a href="'.get_the_permalink().'">';
+						the_post_thumbnail('custom_1', array( 'class'	=> "img-rounded img-responsive center-block featured"));
+						echo '</a>';
+					else:
+						echo '<br />';
+					endif;
+					?>
+					<h2><a href="<?PHP the_permalink(); ?>"><?PHP the_title(); ?></a></h2>
 
-				<div class="article clearfix">
-					<?PHP ($option['show_excerpt_in_lists'] ? the_excerpt() : the_content(false)); ?>
+					<?PHP get_template_part( 'context' ); ?>
+
+					<div class="article clearfix">
+						<?PHP ($option['show_excerpt_in_lists'] ? the_excerpt() : the_content(false)); ?>
+					</div>
+					<div align="right">
+						<a href="<?PHP the_permalink(); ?>" class="btn btn-primary"><?php echo __('Read more...', 'material-for-coders'); ?></a>
+					</div>
 				</div>
-				<div align="right">
-					<a href="<?PHP the_permalink(); ?>" class="btn btn-primary"><?php echo __('Read more...', 'material-for-coders'); ?></a>
-				</div>
+				<br />
+				<hr />
+				<?PHP endwhile; endif; ?>
 			</div>
-			<br />
-			<hr />
-			<?PHP endwhile; endif; ?>
 
 			<?PHP get_template_part('pagination'); ?>
 		</div>
