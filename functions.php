45991140c46c2fd8d3c4b4bc9ef44b72078a9b27
Josep Viciana <emmgfx@gmail.com>
2016-04-02 12:11:43 +0200

    2.0.2: Finally removed the GitHub updater. Use WP directory now.

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once 'includes/thumbnails.php';
-require_once 'includes/updater.php';
 require_once 'includes/settings-page.php';
 require_once 'includes/widgets-and-menus.php';
 require_once 'includes/no-theme-settings.php';
